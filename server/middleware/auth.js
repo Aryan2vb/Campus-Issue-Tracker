06a96c6a1e045e3701a4bd85a2a1c79f3eea8a83
Aryan2vb <aryansoni2105@gmail.com>
2024-11-12 14:09:19 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
     const token = req.header('Authorization')?.split(' ')[1];
+    console.log("Extracted Token:", token);  // For debugging
     if (!token) return res.status(401).send('Access Denied');
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
